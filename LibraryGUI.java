45186f8d0d3f184531f7fb9425d3a10c26a22b10
Ali241124 <ah627449@gmail.com>
2025-06-23 20:03:48 +0500

    LibraryGUI.java

--- LibraryGUI.java
+++ LibraryGUI.java
@@ -2,17 +2,22 @@ package ui;
 
 import service.LibraryService;
 import java.awt.EventQueue;
-import javax.swing.*;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
 import java.awt.Color;
+import javax.swing.JTextArea;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.JScrollBar;
+import javax.swing.JScrollPane;
 
-public class LibraryGUI extends JFrame {
+public class LibraryGUI {
 
 	private LibraryService service = new LibraryService();
-
-	private static final long serialVersionUID = 1L;
-	private JPanel contentPane;
+	private JFrame frmLibraryBookSystem;
 	private JTextField textField;
 	private JTextArea textArea;
 
@@ -23,8 +28,8 @@ public class LibraryGUI extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					LibraryGUI frame = new LibraryGUI();
-					frame.setVisible(true);
+					LibraryGUI window = new LibraryGUI();
+					window.frmLibraryBookSystem.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -33,108 +38,120 @@ public class LibraryGUI extends JFrame {
 	}
 
 	/**
-	 * Create the application
+	 * Create the application.
 	 */
 	public LibraryGUI() {
-		setForeground(new Color(255, 255, 255));
-		setTitle("Basic Library System");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 500, 350); 
-		contentPane = new JPanel();
-		contentPane.setLayout(null);
-		setContentPane(contentPane);
+		initialize();
+	}
 
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frmLibraryBookSystem = new JFrame();
+		frmLibraryBookSystem.setTitle("Library Book System");
+		frmLibraryBookSystem.setBounds(100, 100, 450, 300);
+		frmLibraryBookSystem.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmLibraryBookSystem.getContentPane().setLayout(null);
+		
 		JLabel lblNewLabel = new JLabel("Enter Book Title:");
-		lblNewLabel.setBounds(28, 32, 124, 21);
-		contentPane.add(lblNewLabel);
-
+		lblNewLabel.setBounds(21, 24, 111, 13);
+		frmLibraryBookSystem.getContentPane().add(lblNewLabel);
+		
 		textField = new JTextField();
-		textField.setBounds(136, 33, 200, 19);
-		contentPane.add(textField);
+		textField.setBounds(144, 21, 235, 19);
+		frmLibraryBookSystem.getContentPane().add(textField);
 		textField.setColumns(10);
-
-		JButton btnAdd = new JButton("Add Book");
-		btnAdd.addActionListener(new ActionListener() {
+		
+		JButton btnNewButton = new JButton("Add Book");
+		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				String title = textField.getText().trim();
+				String title = textField.getText();
 				String result = service.addBook(title);
 				textArea.setText(result);
 				textField.setText("");
+
 			}
 		});
-		btnAdd.setBackground(new Color(128, 255, 128));
-		btnAdd.setBounds(10, 78, 103, 21);
-		contentPane.add(btnAdd);
-
-		JButton btnBorrow = new JButton("Borrow Book");
-		btnBorrow.addActionListener(new ActionListener() {
+		btnNewButton.setBackground(new Color(128, 255, 128));
+		btnNewButton.setBounds(21, 71, 96, 21);
+		frmLibraryBookSystem.getContentPane().add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("Borrow Book");
+		btnNewButton_1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				String title = textField.getText();
 				String result = service.borrowBook(title);
 				textArea.setText(result);
 				textField.setText("");
+
 			}
 		});
-		btnBorrow.setBackground(new Color(128, 255, 128));
-		btnBorrow.setBounds(123, 78, 130, 21);
-		contentPane.add(btnBorrow);
-
-		JButton btnReturn = new JButton("Return Book");
-		btnReturn.addActionListener(new ActionListener() {
+		btnNewButton_1.setBackground(new Color(128, 255, 128));
+		btnNewButton_1.setBounds(144, 71, 127, 21);
+		frmLibraryBookSystem.getContentPane().add(btnNewButton_1);
+		
+		JButton btnNewButton_2 = new JButton("Return Book");
+		btnNewButton_2.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				String title = textField.getText().trim();
+				String title = textField.getText();
 				String result = service.returnBook(title);
 				textArea.setText(result);
 				textField.setText("");
+
 			}
 		});
-		btnReturn.setBackground(new Color(128, 255, 128));
-		btnReturn.setBounds(263, 78, 120, 21);
-		contentPane.add(btnReturn);
-
-		JButton btnSearch = new JButton("Search Book");
-		btnSearch.addActionListener(new ActionListener() {
+		btnNewButton_2.setBackground(new Color(128, 255, 128));
+		btnNewButton_2.setBounds(294, 71, 132, 21);
+		frmLibraryBookSystem.getContentPane().add(btnNewButton_2);
+		
+		JButton btnNewButton_3 = new JButton("Search Book");
+		btnNewButton_3.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				String title = textField.getText();
 				String result = service.searchBook(title);
 				textArea.setText(result);
 				textField.setText("");
+
 			}
 		});
-		btnSearch.setBackground(new Color(128, 255, 128));
-		btnSearch.setBounds(10, 120, 140, 21);
-		contentPane.add(btnSearch);
-
-		JButton btnList = new JButton("List Available Books");
-		btnList.addActionListener(new ActionListener() {
+		btnNewButton_3.setBackground(new Color(128, 255, 128));
+		btnNewButton_3.setBounds(21, 115, 127, 21);
+		frmLibraryBookSystem.getContentPane().add(btnNewButton_3);
+		
+		JButton btnNewButton_4 = new JButton("List Available Books");
+		btnNewButton_4.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				String result = service.getAvailableBook();  
+				String title = textField.getText();
+				String result = service.getAvailableBook();
 				textArea.setText(result);
+				textField.setText("");
+
 			}
 		});
-		btnList.setBackground(new Color(128, 255, 128));
-		btnList.setBounds(160, 120, 230, 21);
-		contentPane.add(btnList);
-
-		JLabel lblOutput = new JLabel("Output:");
-		lblOutput.setBounds(10, 160, 59, 13);
-		contentPane.add(lblOutput);
-
+		btnNewButton_4.setBackground(new Color(128, 255, 128));
+		btnNewButton_4.setBounds(174, 115, 205, 21);
+		frmLibraryBookSystem.getContentPane().add(btnNewButton_4);
+		
+		JLabel lblNewLabel_1 = new JLabel("Output:");
+		lblNewLabel_1.setBounds(21, 155, 75, 13);
+		frmLibraryBookSystem.getContentPane().add(lblNewLabel_1);
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(97, 162, 240, 60);
+		frmLibraryBookSystem.getContentPane().add(scrollPane);
+		
 		textArea = new JTextArea();
-		textArea.setBounds(66, 180, 390, 80);
-		JScrollPane scrollPane = new JScrollPane(textArea);
-		scrollPane.setBounds(66, 180, 390, 80);
-		contentPane.add(scrollPane);
-
-		JButton btnClear = new JButton("Clear");
-		btnClear.addActionListener(new ActionListener() {
+		scrollPane.setViewportView(textArea);
+		
+		JButton btnNewButton_5 = new JButton("Clear");
+		btnNewButton_5.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				textField.setText("");
 				textArea.setText("");
 			}
 		});
-		btnClear.setBackground(new Color(128, 255, 255));
-		btnClear.setBounds(200, 270, 85, 21);
-		contentPane.add(btnClear);
+		btnNewButton_5.setBackground(new Color(128, 255, 255));
+		btnNewButton_5.setBounds(198, 232, 85, 21);
+		frmLibraryBookSystem.getContentPane().add(btnNewButton_5);
 	}
 }
